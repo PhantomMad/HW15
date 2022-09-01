8f7bb620b2c29e03d7164c3bcb935593ce9e4cc9
AndrewBS <phantom@bstal.ru>
2022-09-01 09:28:10 +0300

    Prepare maven and git

--- terra_build/deployment.sh
+++ terra_build/deployment.sh
@@ -1,18 +1,24 @@
 #!/usr/bin/env bash
 set -u
 ami=$(whoami)
-maven_arch="apache-maven-3.8.6-bin.tar.gz"
+git_url="$1"
+maven_arch="apache-maven-*-bin.tar.gz"
 maven_src="/tmp/${maven_arch}"
 maven_dir="/opt/maven"
+git_dir="/tmp/git"
 [ "${ami}" == "root" ] || exit 1
 apt-get update > /dev/null 2>&1
 apt-get install -y default-jdk git tar > /dev/null 2>&1 || exit 1
 rm -rf /var/lib/apt/lists/*
 [ -e "${maven_src}" ] || exit 1
-([ ! -d "${maven_dir}" ] && mkdir ${maven_dir} 2> /dev/null) || exit 1
+([ -d "${git_dir}" ] || mkdir ${git_dir} 2> /dev/null) || exit 1
+([ -d "${maven_dir}" ] || mkdir ${maven_dir} 2> /dev/null) || exit 1
 tar xpvf "${maven_src}" -C "${maven_dir}" --strip-components=1 > /dev/null 2>&1
 export M2_HOME=/opt/maven
 export MAVEN_HOME=/opt/maven
 ln -s ${maven_dir}/bin/mvn /bin/mvn
+git clone "${git_url}" "${git_dir}"
+cd "${maven_dir}"
+mvn clean package
 
 
